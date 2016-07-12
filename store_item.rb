5c52e391d49f74f5b17500f0e831e4210927bb32
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-07-11 20:03:12 -0700

    initialize git, create store_item.rb with guitar models

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+#guitar store
+
+guitar1 = {brand: "Gibson", model: "Les Paul", color: "Sunburst"}
+
+guitar2 = {brand: "Ibanez", model: "JEM", color: "White"}
+
+guitar3 = {brand: "Fender", model: "Yngwie Malmsteen Signature", color: "Vintage Yellow"}
